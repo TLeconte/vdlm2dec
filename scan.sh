f068e8f6dbe14722b58e5efba0af907b37c28fa3
Thierry Leconte <thierry.leconte@laposte.net>
2018-03-11 17:01:52 +0100

    show messages histogram

---
+++ scan.sh
@@ -0,0 +1,16 @@
+#/bin/bash
+
+let fm=130
+while [ "$fm" -lt 137 ] ; do
+let f0=0; let f1=125 ; let f2=250 ; let f3=375 ; let f4=500 ; let f5=625 ; let f6=750 ; let f7=875 
+echo $fm
+while [ "$f0" -lt 125 ] ; do
+	echo $f0
+	./vdlm2dec -g 15 -l scanlog  $fm.$f0 $fm.$f1 $fm.$f2 $fm.$f3 $fm.$f4 $fm.$f5 $fm.$f6 $fm.$f7 &
+	sleep 500
+	killall vdlm2dec
+	let f0=f0+25;let f1=f1+25;let f2=f2+25;let f3=f3+25; f4=f4+25;let f5=f5+25;let f6=f6+25;let f7=f7+25;
+ done
+ let fm=fm+1;
+done
+grep -e "(F:" scanlog | cut -c 8-14 | sort | uniq -c
